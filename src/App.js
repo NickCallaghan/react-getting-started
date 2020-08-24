e10d7a512ebe13a42b50b3e706b5a085897918e3
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-25 00:50:59 +0100

    login form created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
+import LoginForm from "./components/LoginForm.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleCount = (increment = 1) => {
-    let newCount = count + increment;
-    setCount(newCount);
-  };
-
   return (
     <div className="App">
-      <Button className="m-3" variant="primary" onClick={() => handleCount(-1)}>
-        Down
-      </Button>
-      <span className="count">{count}</span>
-      <Button className="m-3" variant="primary" onClick={() => handleCount(1)}>
-        Up
-      </Button>
+      <LoginForm />
     </div>
   );
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const LoginForm = () => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("You form has been submitted");
+    //We'll add the form handler later
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
