02975ce6d7e69c402d83f25509d40f84d30b0340
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-25 00:24:54 +0100

    Adding counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = (increment = 1) => {
+    let newCount = count + increment;
+    setCount(newCount);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome the the session!
-        </a>
-      </header>
+      <Button className="m-3" variant="primary" onClick={() => handleCount(-1)}>
+        Down
+      </Button>
+      <span className="count">{count}</span>
+      <Button className="m-3" variant="primary" onClick={() => handleCount(1)}>
+        Up
+      </Button>
     </div>
   );
 }
