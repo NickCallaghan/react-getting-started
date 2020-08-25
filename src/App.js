6e27c1647969b77207b739f41bbb833aa0ef041d
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-08-25 01:20:18 +0100

    rendering the home component if logged in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LoginForm from "./components/LoginForm.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
   const [user, setUser] = useState();
@@ -9,11 +10,15 @@ function App() {
     // Set the logged in user
     setUser(user);
   };
-  return (
-    <div className="App">
-      <LoginForm welcomeMessage="React Login" handleLogin={handleLogin} />
-    </div>
-  );
+  if (!user) {
+    return (
+      <div className="App">
+        <LoginForm welcomeMessage="React Login" handleLogin={handleLogin} />
+      </div>
+    );
+  }
+  // Return the home page if the user is logged in
+  return <Home user={user} />;
 }
 
 export default App;
